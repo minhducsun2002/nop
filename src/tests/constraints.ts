955dc42b5d884e62facfd2a44430710bfff78a4b
Minh Duc Vo <minhducsun2002@gmail.com>
2020-04-08 18:15:37 +0700

    Guard against improper environment variable name

--- src/tests/constraints.ts
+++ src/tests/constraints.ts
@@ -17,6 +17,8 @@ export function validate(c : any) {
     if (v.isArray(c.env) || (!v.isObject(c.env)))
         throw new TypeError(`Invalid test constraints : env must be an object!`)
     let { env } = c as { [id: string]: any };
+    for (let k in env) if (!/[a-zA-Z_]+[a-zA-Z0-9_]*/.test(k))
+        throw new TypeError(`Invalid test constraints : env var name must be valid. Check "${k}" variable.`)
     for (let k in env) if (!v.isString(env[k]))
         throw new TypeError(`Invalid test constraints : env must map a string to a string. Check "${k}" variable.`)
 }
